734839b651f9dc49b23899fd0a1d733ec0151b01
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-10-24 19:49:01 -0300

    WebSockets integrados al chat, falta nickname

--- app/scripts/controllers/Almacen/ChatCtrl.js
+++ app/scripts/controllers/Almacen/ChatCtrl.js
@@ -5,19 +5,33 @@ angular.module('sapoApp')
   .controller('ChatCtrl', [ '$scope', 'toastr',
     function ($scope, toastr) {
 
+      wsUrl = 'ws:sapo-backendrs.rhcloud.com:8000/openshiftproject/chat/Ale';
+
+      console.log('WebSockets Url : ' + wsUrl);
+      ws = new WebSocket(wsUrl);
+
+      ws.onopen = function(event){
+        console.log('WebSocket connection started');
+      };
+
+      ws.onclose = function(event){
+        console.log("Remote host closed or refused WebSocket connection");
+        console.log(event);
+      };
+
+      ws.onmessage = function(event){
+        console.log(event.data);
+        $scope.chatCtrl.messages.push({
+          username: 'Remote',
+          text: event.data,
+          date: Date()
+        });
+        $scope.$apply();
+      };
+
+
       this.init = function () {
-        this.messages = [
-          {
-            username: 'Ale',
-            text: 'Hola',
-            date: Date()
-          },
-          {
-            username: 'Jhon',
-            text: 'Chau',
-            date: Date()
-          }
-        ];
+        this.messages = [];
         $scope.messages = this.messages;
       };
 
@@ -26,12 +40,14 @@ angular.module('sapoApp')
       this.mandarMensaje = function () {
         console.log("quiere mandar mensaje");
         console.log($scope);
-        var msg = {
+        var text = $scope.chatCtrl.writingMessage,
+          msg = {
           username: 'Yo',
-          text: $scope.chatCtrl.writingMessage,
+          text: text,
           date: Date()
         };
         $scope.chatCtrl.writingMessage = '';
         $scope.chatCtrl.messages.push(msg);
+        ws.send(text);
       }
     }]);
