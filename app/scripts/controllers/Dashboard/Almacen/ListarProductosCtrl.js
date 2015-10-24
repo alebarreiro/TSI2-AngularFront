ab9a555ee5e8b8fc4a876593636cb22935481267
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-10-23 23:02:12 -0300

    listado de productos al seleccionar cat

--- app/scripts/controllers/Dashboard/Almacen/ListarProductosCtrl.js
+++ app/scripts/controllers/Dashboard/Almacen/ListarProductosCtrl.js
@@ -2,28 +2,62 @@
  * Created by alejandrobarreiro on 23/10/15.
  */
 angular.module('sapoApp')
-  .controller('ListarProductosCtrl', [ '$scope', 'CategoriaHandler', function ($scope, CategoriaHandler) {
+  .controller('ListarProductosCtrl', ['$scope', 'CategoriaHandler', 'categoriaService',
+    function ($scope, CategoriaHandler, categoriaService) {
 
-    this.init = function() {
+      var arrayProductos = [];
 
-      console.log('init');
-      var catHandler = new CategoriaHandler();
-      $scope.categorias = [];
-      var idCatsElegidas = $scope.$parent.almacen.categorias;
+      this.init = function () {
 
-      idCatsElegidas.forEach(function (c) {
-        console.log('iterando ' + c);
-        var cat = catHandler.getCategoria(c);
-        if (cat) $scope.categorias.push(cat);
-      });
+        var that = this;
 
-      console.log($scope.categorias);
-    };
+        arrayProductos = [];
+        var catHandler = new CategoriaHandler();
+        $scope.categorias = [];
+        var idCatsElegidas = $scope.$parent.almacen.categorias;
+        idCatsElegidas.forEach(function (c) {
+          console.log('iterando ' + c);
+          var cat = catHandler.getCategoria(c);
+          if (cat) {
+            $scope.categorias.push(cat);
+            arrayProductos[cat] = [];
+          }
+        });
+      };
 
-    $scope.listarProductos = function (id) {
-      console.log("holaaa");
-      $scope.catId = id;
-    };
+      $scope.listarProductos = function (id) {
+        var that = this;
+        console.log("holaa listar PRoductos");
+        if (arrayProductos[id]) {
+          console.log('prods cacheados');
+          console.log(arrayProductos[id]);
+          $scope.productos = arrayProductos[id];
+        } else {
+          categoriaService.getProductosCategoria(id).then(function (productos) {
 
-    this.init();
-  }]);
+            var productosConStock = [];
+            productos.forEach(function (p) {
+              p.stock = 0;
+              p.catProd = id;
+              productosConStock.push(p);
+            });
+            console.log(productosConStock);
+            $scope.productos = productosConStock;
+            arrayProductos[id] = productosConStock;
+          });
+        }
+        $scope.catId = id;
+
+      };
+
+      $scope.agregarStockProducto = function (catProd, idProducto) {
+        console.log(arrayProductos[catProd]);
+      };
+
+      $scope.sacarStockProducto = function (catProd, idProducto) {
+        console.log(arrayProductos[catProd]);
+        //this.arrayProductos[idProducto].stock = (this.arrayProductos[idProducto] && this.arrayProductos[idProducto].stock > 0) ?  this.arrayProductos[idProducto].stock-- : 0;
+      };
+
+      this.init();
+    }]);
