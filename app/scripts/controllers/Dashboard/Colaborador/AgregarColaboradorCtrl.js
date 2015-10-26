26bcbe3414ab086a4b745cdfdaef944ed36056fa
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-10-25 23:03:01 -0300

    watcher para la busqueda del user

--- app/scripts/controllers/Dashboard/Colaborador/AgregarColaboradorCtrl.js
+++ app/scripts/controllers/Dashboard/Colaborador/AgregarColaboradorCtrl.js
@@ -2,39 +2,22 @@
  * Created by alejandrobarreiro on 25/10/15.
  */
 angular.module('sapoApp')
-  .controller('AgregarColaboradorCtrl', ['$scope', 'almacenService', 'toastr', function ($scope, almacenService, toastr) {
+  .controller('AgregarColaboradorCtrl', ['$scope', 'usuarioService', 'toastr', function ($scope, usuarioService) {
 
-    $scope.almacen = {};
-    $scope.noTemplateSelected = true;
+    $scope.usuario = {};
 
     this.init = function () {
 
     };
 
-    $scope.seleccionarTemplate = function (idTemplate) {
-      console.log("Seleccionaste: " + idTemplate);
-      $scope.almacen.templateElegido = idTemplate;
+    $scope.$watch('usuario.id', function() {
+
+      
+    });
+
+    $scope.agregarColaborador = function () {
+
     };
 
-    $scope.crearAlmacen = function () {
-      console.log($scope.almacen);
-
-      var almacen = $scope.almacen;
-
-      if (almacenService.validarAlmacen(almacen)) {
-        almacenService.crearNuevaAlmacen(almacen)
-          .then(function (a) {
-            console.log(a);
-            $scope.almacen.id = a.id;
-            toastr.success('Datos del almacen confirmados');
-          })
-          .catch(function () {
-            toastr.error('Hubo un error al crear el almacen.')
-          });
-      } else {
-        toastr.warning('Por favor, ingrese los datos del almacen.');
-      }
-    },
-
-      this.init();
+    this.init();
   }]);
