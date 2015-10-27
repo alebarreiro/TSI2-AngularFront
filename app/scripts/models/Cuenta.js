3f4b61c3ec7c57509fef8f1f568fed27f6e2e0a0
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-10-27 19:46:55 -0300

    servicios para la cuenta

--- app/scripts/models/Cuenta.js
+++ app/scripts/models/Cuenta.js
@@ -8,9 +8,9 @@ angular.module('sapoApp')
     var API_REST_URL = REST_API.BASE_URL;
 
     return $resource(API_REST_URL + ':resource/:subresource/:userid/:id', { userid: '@userid', id: '@id' }, {
-      getCuentas             : { method: 'get',  params: { resource: 'cuentas' },                           isArray: true  },
-      crearCuenta            : { method: 'post', params: { resource: 'cuentas', subresource: 'create' },    sArray: false  },
-      actualizarCuenta       : { method: 'put',  params: { resource: 'cuentas', subresource: 'update' },    isArray: false },
-      agregarUsuarioCuenta   : { method: 'put',  params: { resource: 'cuentas', subresource: 'update' },    isArray: false },
+      getCuentas              : { method: 'get',  params: { resource: 'cuentas' },                           isArray: true  },
+      crearCuenta             : { method: 'post', params: { resource: 'cuentas', subresource: 'create' },    sArray: false  },
+      actualizarCuenta        : { method: 'put',  params: { resource: 'cuentas', subresource: 'update' },    isArray: false },
+      actualizarCuentaUsuario : { method: 'put',  params: { resource: 'cuentas', subresource: 'update' },    isArray: false },
     })
   }]);
---
+++ app/scripts/services/CuentaService.js
@@ -0,0 +1,60 @@
+/**
+ * Created by alejandrobarreiro on 27/10/15.
+ */
+angular.module('sapoApp')
+  .service('cuentaService', ['$q',  'Cuenta', 'authService', function ($q, Cuenta, authService) {
+
+    this.init = function () {};
+
+    this.getCuentas = function () {
+
+      var deferred = $q.defer();
+      Cuenta.getCuentas({}, {}, function (cuentas) {
+        deferred.resolve(cuentas);
+      }, function (error) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    };
+
+    this.crearCuenta = function (data) {
+
+      var deferred = $q.defer();
+      Cuenta.crearCuenta({}, data, function (cuenta) {
+        deferred.resolve(cuenta);
+      }, function (error) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    };
+
+    this.actualizarCuenta = function (data) {
+
+      var deferred = $q.defer();
+
+      Cuenta.actualizarCuenta({id: data.id}, data, function (cuenta) {
+        deferred.resolve(cuenta);
+      }, function (error) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    };
+
+    /**
+     * Cambio de plan (cuenta) al usuario.
+     * @param idCuenta id de la cuenta del nuevo plan.
+     * @returns {*}
+     */
+    this.actualizarCuentaUsuario = function (idCuenta) {
+
+      var deferred = $q.defer(),
+        user = authService.getLoggedUser();
+      Cuenta.actualizarCuentaUsuario({userid: user.id, id: idCuenta}, {}, function (cuenta) {
+        deferred.resolve(cuenta);
+      }, function (error) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    };
+
+  }]);
