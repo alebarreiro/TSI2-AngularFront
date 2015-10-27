0443a1cf12810747d40a2eee4975dff0714348e2
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-10-27 19:35:31 -0300

    agregado resource cuenta

---
+++ app/scripts/models/Cuenta.js
@@ -0,0 +1,16 @@
+/**
+ * Created by alejandrobarreiro on 8/10/15.
+ */
+'use strict';
+
+angular.module('sapoApp')
+  .factory('Cuenta', ['$http', '$resource', 'REST_API', function($http, $resource, REST_API) {
+    var API_REST_URL = REST_API.BASE_URL;
+
+    return $resource(API_REST_URL + ':resource/:subresource/:userid/:id', { userid: '@userid', id: '@id' }, {
+      getCuentas             : { method: 'get',  params: { resource: 'cuentas' },                           isArray: true  },
+      crearCuenta            : { method: 'post', params: { resource: 'cuentas', subresource: 'create' },    sArray: false  },
+      actualizarCuenta       : { method: 'put',  params: { resource: 'cuentas', subresource: 'update' },    isArray: false },
+      agregarUsuarioCuenta   : { method: 'put',  params: { resource: 'cuentas', subresource: 'update' },    isArray: false },
+    })
+  }]);
