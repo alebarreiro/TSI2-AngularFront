d450420b97d3e9969d3beaafbf446d3c3a073498
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-10-27 22:37:16 -0300

    controller y vista con logica para hacer almacen privada/publica

--- app/scripts/controllers/Dashboard/Almacen/ListarAlmacenesCtrl.js
+++ app/scripts/controllers/Dashboard/Almacen/ListarAlmacenesCtrl.js
@@ -2,10 +2,41 @@
  * Created by alejandrobarreiro on 8/10/15.
  */
 angular.module('sapoApp')
-  .controller('ListarAlmacenesCtrl', ['almacenes',  function (almacenes) {
+  .controller('ListarAlmacenesCtrl', ['almacenes', '$scope', '$state', 'almacenService', 'toastr',
+    function (almacenes, $scope, $state, almacenService, toastr) {
 
     this.init = function() {
-      this.almacenes = almacenes;
+      $scope.almacenes = almacenes;
+    };
+
+    $scope.hacerPrivadaAlmacen = function (almacenId) {
+
+      var data = {
+        privado: true
+      };
+
+      almacenService.actualizarAlmacen(almacenId, data)
+        .then(function(){
+          toastr.success("Almacen actualizada con exito!");
+        })
+        .catch(function(){
+          toastr.warning("No es posible completar tu solicitud.");
+        });
+    };
+
+    $scope.hacerPublicaAlmacen = function (almacenId) {
+
+      var data = {
+        privado: false
+      };
+
+      almacenService.actualizarAlmacen(almacenId, data)
+        .then(function(){
+          toastr.success("Almacen actualizada con exito!");
+        })
+        .catch(function(){
+          toastr.warning("No es posible completar tu solicitud.");
+        });
     };
 
     this.init();
