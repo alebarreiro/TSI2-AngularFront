3c9bb9b7abeb5a3f8edc55549976ecf06b61c57e
Ubuntu TSI2 <alejandroanonmallo@gmail.com>
2015-11-02 15:55:27 -0300

    Se modifica el stock en la vista del almacén.

--- app/scripts/controllers/Almacen/MostrarAlmacenCtrl.js
+++ app/scripts/controllers/Almacen/MostrarAlmacenCtrl.js
@@ -1,6 +1,6 @@
 angular.module('sapoApp')
-  .controller('MostrarAlmacenCtrl', ['almacen', '$scope', 'almacenService', 'toastr', 'lodash', 'ProductoHandler',
-    function (almacen, $scope, almacenService, toastr, lodash, ProductoHandler) {
+  .controller('MostrarAlmacenCtrl', ['almacen', '$scope', 'almacenService', 'toastr', 'lodash', 'ProductoHandler', 'CategoriaHandler',
+    function (almacen, $scope, almacenService, toastr, lodash, ProductoHandler, CategoriaHandler) {
 
     this.init = function () {
       console.log(almacen);
@@ -9,13 +9,31 @@ angular.module('sapoApp')
       $scope.productos = almacen.stockproductos;
       $scope.categorias = almacen.categorias;
       $scope.colaboradores = almacen.colaboradores;
+        console.log($scope.productos);
+        console.log($scope.categorias);
+
     };
 
     this.init();
 
-    $scope.agregarStockProducto = function (idProducto) {
+    $scope.aumentarStockProducto = function (idProducto) {
 
+      var index = lodash.findIndex($scope.productos, function(prod) {
+          return prod.productoID == idProducto;
+      });
+      console.log(index)
 
-      $scope.productos[idProducto].cantidad ++;
+    $scope.productos[index].cantidad++
     };
+
+    $scope.reducirStockProducto = function (idProducto) {
+
+        var index = lodash.findIndex($scope.productos, function(prod) {
+            return prod.productoID == idProducto;
+        });
+        console.log(index)
+        if ($scope.productos[index].cantidad > 0)
+            $scope.productos[index].cantidad--;
+    };
+
   }]);
