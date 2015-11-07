8afcf8232c1a23f1e65ec85e2e85f35b7ce35373
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-11-07 20:59:58 -0300

    Servicios y resources para obtener notificaciones

---
+++ app/scripts/models/Notificacion.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alejandrobarreiro on 8/10/15.
+ */
+'use strict';
+
+angular.module('sapoApp')
+  .factory('Notificacion', ['$http', '$resource', 'REST_API', function($http, $resource, REST_API) {
+    var API_REST_URL = REST_API.BASE_URL;
+
+    return $resource(API_REST_URL + ':resource/:module/:id/:op/:subop/:userid', { id: '@id', userid: '@userid' }, {
+      getNotificacionesStock             : { method: 'get',  params: { resource: 'notificaciones', op: 'stock' },         isArray: true },
+      getNotificacionesLimiteCuenta      : { method: 'get',  params: { resource: 'notificaciones', op: 'limitecuenta' },  isArray: true },
+    })
+  }]);
---
+++ app/scripts/services/NotificacionesService.js
@@ -0,0 +1,32 @@
+/**
+ * Created by alejandrobarreiro on 8/10/15.
+ */
+angular.module('sapoApp')
+  .service('notificacionesService', ['$q', 'Almacen', 'Usuario', 'authService', 'Notificacion', function ($q, Almacen, Usuario, authService, Notificacion) {
+
+    this.init = function () {};
+
+    this.getNotificacionesStock = function (idAlmacen) {
+      var deferred = $q.defer();
+      Notificacion.getNotificacionesStock({id: idAlmacen}, {},
+        function (result) {
+          deferred.resolve(result);
+        }, function (error) {
+          deferred.reject(error);
+        });
+      return deferred.promise;
+    };
+
+    this.getNotificacionesLimiteCuenta = function () {
+      var user = authService.getLoggedUser();
+      
+      var deferred = $q.defer();
+      Notificacion.getNotificacionesLimiteCuenta({userid: user.id}, {},
+        function (result) {
+          deferred.resolve(result);
+        }, function (error) {
+          deferred.reject(error);
+        });
+      return deferred.promise;
+    };
+  }]);
